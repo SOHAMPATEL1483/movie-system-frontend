532a257e5fc3093fac89fe3850a28be6b5096d02
SOHAMPATEL1483 <86504280+SOHAMPATEL1483@users.noreply.github.com>
2023-03-15 20:20:38 +0530

    login page, hero section

---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { Card, Button, Toggle } from "flowbite-svelte";
+  let vCard = false;
+</script>
+
+<section class="dark:bg-gray-900 dark:text-gray-100">
+  <div
+    class="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+    <div
+      class="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+      <p class="text-2xl font-bold font-['Open_Sans'] leading-none sm:text-6xl">
+        Find Your<br />
+        <span class="dark:text-violet-400"> Next </span> Movie Obsession
+        <span class="dark:text-violet-400" />
+      </p>
+      <p class="mt-6 mb-8 text-lg sm:mb-12">
+        Discover your next cinematic adventure with our
+        <br class="hidden md:inline lg:hidden" />
+        expertly curated movie recommendations
+      </p>
+    </div>
+    <div
+      class="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+      <img
+        src="/hero3.svg"
+        alt=""
+        class="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+    </div>
+  </div>
+</section>
+
+<div
+  class="w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <a href="/">
+    <img
+      class="rounded-t-lg h-70"
+      src="https://m.media-amazon.com/images/M/MV5BNjc0NGU1YjItZGIxZS00MjMxLTlmZmYtNWUyMWRmY2Y2NDA0XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"
+      alt="" />
+  </a>
+  <div class="p-2 text-center">
+    <a href="/">
+      <h5
+        class="mb-2 text-lg font-bold  font-['Open_Sans'] tracking-tight text-gray-900 dark:text-white">
+        Final Fantasy
+      </h5>
+    </a>
+  </div>
+</div>
+
+<style>
+  :global() {
+    background: #111827;
+  }
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,152 @@
+<script>
+  import { Card, Button, Label, Input, Checkbox } from "flowbite-svelte";
+  import ls from "localstorage-slim";
+  // 43200
+  let username = "";
+  let password = "";
+
+  let login = true;
+
+  const toggle_login = () => {
+    login = !login;
+    (username = ""), (password = "");
+  };
+  const signin = async () => {
+    let res = await fetch("http://localhost:8000/api/token/", {
+      method: "POST",
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    let token = await res.json();
+    if (res.ok) {
+      console.log(token);
+      ls.set("jwt", token.access, { ttl: 43200 });
+      // @ts-ignore
+      window.location = "/";
+    } else {
+      console.log(token.detail);
+      if (token.username) console.log("username: " + token.username[0]);
+      if (token.password) console.log("password: " + token.password[0]);
+      ls.clear();
+    }
+    (username = ""), (password = "");
+  };
+  const signup = async () => {
+    let res = await fetch("http://localhost:8000/user/create/", {
+      method: "POST",
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.ok) console.log("successfully created user");
+    else console.log("failed to create user");
+    toggle_login();
+  };
+  const signout = () => {
+    ls.clear();
+  };
+</script>
+
+<!-- {#if login} -->
+<!-- <h1>signin</h1> -->
+<!-- <input type="text" placeholder="Username" bind:value={username} /> -->
+<!-- <input type="password" placeholder="Password" bind:value={password} /> -->
+<!-- <button on:click={signin}>signin</button> -->
+<!-- <p>Don't have Account?</p> -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- <p on:click={toggle_login}>Signup</p> -->
+<!-- {:else} -->
+<!-- <h1>signup</h1> -->
+<!-- <input type="text" placeholder="Username" bind:value={username} />
+    <input type="password" placeholder="Password" bind:value={password} />
+    <button on:click={signup}>signup</button>
+    <p>Already have Account?</p>
+    svelte-ignore a11y-click-events-have-key-events
+    <p on:click={toggle_login}>Signin</p> -->
+<!-- {/if} -->
+
+<!-- <div>
+    <button on:click={signout}>signout</button>
+</div> -->
+{#if login}
+  <!-- signin -->
+  <div class="grid h-screen place-items-center bg-gray-900">
+    <div class="w-96">
+      <Card>
+        <form class="flex flex-col space-y-6" action="/">
+          <h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">
+            Sign in to our platform
+          </h3>
+          <Label class="space-y-2">
+            <span>Username</span>
+            <Input
+              bind:value={username}
+              type="text"
+              name="email"
+              placeholder="xyz123"
+              required />
+          </Label>
+          <Label class="space-y-2">
+            <span>Your password</span>
+            <Input
+              bind:value={password}
+              type="password"
+              name="password"
+              placeholder="•••••"
+              required />
+          </Label>
+          <Button on:click={signin} class="w-full"
+            >Login to your account</Button>
+          <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Not registered? <span
+              on:click={toggle_login}
+              class="text-blue-700 hover:underline dark:text-blue-500"
+              >Create account</span>
+          </div>
+        </form>
+      </Card>
+    </div>
+  </div>
+{:else}
+  <div class="grid  h-screen place-items-center bg-gray-900">
+    <div class="w-96">
+      <Card>
+        <form class="flex flex-col space-y-6" action="/">
+          <h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">
+            Create a new account
+          </h3>
+          <Label class="space-y-2">
+            <span>Username</span>
+            <Input
+              bind:value={username}
+              type="text"
+              name="email"
+              placeholder="xyz123"
+              required />
+          </Label>
+          <Label class="space-y-2">
+            <span>Your password</span>
+            <Input
+              bind:value={password}
+              type="password"
+              name="password"
+              placeholder="•••••"
+              required />
+          </Label>
+          <Button on:click={signup} class="w-full">Create Account</Button>
+          <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            Already have an account?
+            <span
+              on:click={toggle_login}
+              class="text-blue-700 hover:underline dark:text-blue-500">
+              Sign in</span>
+          </div>
+        </form>
+      </Card>
+    </div>
+  </div>
+{/if}
